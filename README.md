804bbc71265a913ec89109209fcb31c73d453264
Ben Taylor <bentaylor@knights.ucf.edu>
2019-10-26 16:46:55 -0400

    Fixed formatting, added comments

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Word-Guess-Game
+
+What the project does
+The Word Guess Game is essentially hang man. There is a pool of words that the user will try to guess with a limited amount of attempts. The user presses a letter key and if it is found within the word, it populates on the screen in the guessing word as well as letters already guessed.
+Why the project is useful
+This project is useful to showcase various javascript functionality.
+How users can get started with the project
+Press any letter key to play.
+
+github.spartancarm1ne.io/word-guess-game
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
-var userWins = 0;
-var numGuessLeft = 15;
-var stopInput = false;
-var wrongLetters = [];
-var correctLetters = [];
-var wordToGuess= [];
+
+//Variable declarations
+var userWins = 0; //keeps track of wins
+var numGuessLeft = 15; //keeps track of guesses left
+var stopInput = false; //pauses game
+var wrongLetters = []; //array of letters pressed
+var correctLetters = []; //array of letters correctly pressed
+var wordToGuess= []; 
 var word;
 
-
+//variables to update text on screen
 var userInputKeyText = document.getElementById("user-guess");
 var userWinsText = document.getElementById("num-wins");
 var wordToGuessText = document.getElementById("word");
@@ -14,69 +16,81 @@ var numOfGuessLeftText = document.getElementById("num-of-guess");
 var changeImage = document.getElementById("imageDiv");
 var playSong = document.getElementById("music");
 
+//array containing words to guess
 var wordList = ["arya", "stark", "baratheon", "john", "tyrion", "ned", "targaryen", "greyjoy", "daenarys", "lannister", "snow", "joffrey", "drogo", "stannis", "bronn", "margaery", "tyrell", "melisandre"];
 
+//function call to make sure game is ready to go
 restartGame();
-playMusic();
 
-wordToGuessText.textContent = wordToGuess.join("");
 
+//function looking for key press
 document.onkeyup = function(event) {
+    //check to see if the game is paused
     if (!stopInput){
+        //check that keys pressed are a-z lowercase
         if (event.keyCode >= 65 && event.keyCode <= 90){
-    var userGuess = event.key;
-    console.log(userGuess);
-    console.log("Correct Letters " + correctLetters);
-    console.log("Wrong letters" + wrongLetters);
+         //set the key pressed to userGuess var
+        var userGuess = event.key;
+        console.log(userGuess);
+        console.log("Correct Letters " + correctLetters);
+        console.log("Wrong letters" + wrongLetters);
 
-    for(var i = 0; i < word.length; i++){
+            //for loop to iterate through the word to guess and check the user key press is found within the word selected from wordList array
+            for(var i = 0; i < word.length; i++){
 
-     if( userGuess === word[i]){
-        correctLetters[i] = event.key;
-        console.log(correctLetters);
-        wordToGuess[i] = userGuess;
-        wordToGuessText.textContent = wordToGuess.join("");
-        if(!correctLetters.includes(userGuess)){
-            userInputKeyText.textContent += event.key + " ";
+                //logic to find a letter that was pressed inside the word
+                if( userGuess === word[i]){
+                    //add the correct letter to the array
+                    correctLetters[i] = event.key;
+                    console.log(correctLetters);
+                    //add the correct letter to the array and write it to the screen
+                    wordToGuess[i] = userGuess;
+                    wordToGuessText.textContent = wordToGuess.join("");
+                    //checks to see if the user already guessed the same letter
+                    if(!correctLetters.includes(userGuess)){
+                        userInputKeyText.textContent += event.key + " ";
+                    }
+                    //checks to see if the user has guessed all correct letters, stops the game from taking in input, increases the wins on the screen, then resets the game after 2 seconds.
+                    if (correctLetters.join("") === word){
+                        stopInput = true;
+                        userWins++;
+                        userWinsText.textContent = userWins;
+                        setTimeout(restartGame, 2000);
+                    }
+                }
+            }
+    
+            //logic for when user presses a letter not in the word
+            if (userGuess !== word[i]){
+                //adds wrong letter in to the wrongLetter array, adds the letter to the letters guessed on screen, subtracts the number of guesses 
+                if(!wrongLetters.includes(userGuess)){
+                wrongLetters.push(userGuess)
+                userInputKeyText.textContent += event.key + " ";
+                numGuessLeft--;
+                console.log(wrongLetters);
+                }
+                // checks to see if there are zero guesses left, stops input, shows the correct word, and restarts game after 2 seconds.
+                if (numGuessLeft === 0){
+                    stopInput=true;
+                    wordToGuessText.textContent = word;
+                    setTimeout(restartGame, 2000);
+                }
+            }
+    
+            //updates how many guesses left on screen
+            numOfGuessLeftText.textContent = numGuessLeft;
+    
         }
-        if (correctLetters.join("") === word){
-            stopInput = true;
-            
-            userWins++;
-            userWinsText.textContent = userWins;
-            setTimeout(restartGame, 2000);
-        }
-     }
-    }
-    
-     if (userGuess !== word[i]){
-         if(!wrongLetters.includes(userGuess)){
-             wrongLetters.push(userGuess)
-             userInputKeyText.textContent += event.key + " ";
-             numGuessLeft--;
-             console.log(wrongLetters);
-         }
-         if (numGuessLeft === 0){
-            stopInput=true;
-            wordToGuessText.textContent = word;
-            setTimeout(restartGame, 2000);
-         }
-     }
-    
-    numOfGuessLeftText.textContent = numGuessLeft;
-    
-}
     }
 }
 
+//Initially sets the number wins on screen to zero
 userWinsText.textContent = userWins;
 
-function playMusic() {
-    playSong.play();
-}
 
-
+//function to reset the game after a loss or win.
 function restartGame (){
+    //resetting variables
     numGuessLeft = 15;
     correctLetters = [];
     wrongLetters = [];
@@ -84,14 +98,16 @@ function restartGame (){
     stopInput= false;
     userInputKeyText.textContent = "";
     numOfGuessLeftText.textContent = numGuessLeft;
-    
+    //randomly selecting a word within the wordList array
     word = wordList[Math.floor(Math.random() * wordList.length)];
 
     console.log(word);
     
+    //Pushing the appropriate number of dashes into the wordToGuess array
     for (var i = 0; i < word.length; i++){
         
-            wordToGuess.push("-");
+        wordToGuess.push("-");
     }
+    //writing the dashes on to the screen.
     wordToGuessText.textContent = wordToGuess.join("");
-    }
+}
